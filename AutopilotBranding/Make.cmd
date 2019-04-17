70a9828ed5ebdfaa08e9c595fa68377980adc20f
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-04-17 01:48:06 -0400

    full path refs
    Replaced ".." with "C:\Program Files\PackageManagement\NuGet\" to allow for AutopilotBranding folder to be independent of Wix install location.

--- AutopilotBranding/Make.cmd
+++ AutopilotBranding/Make.cmd
@@ -3,5 +3,5 @@ rmdir obj\Debug /s /q
 rmdir bin\Debug /s /q
 
 :: Compile and link
-"%Wix%\bin\candle.exe" -out obj\Debug\ -ext ..\packages\PowerShellWixExtension.2.0.1\tools\lib\PowerShellWixExtension.dll Product.wxs
-"%Wix%\bin\light.exe" -out bin\Debug\AutopilotBranding.msi -ext ..\packages\PowerShellWixExtension.2.0.1\tools\lib\PowerShellWixExtension.dll obj\Debug\Product.wixobj
+"%Wix%\bin\candle.exe" -out obj\Debug\ -ext "%ProgramFiles%\PackageManagement\NuGet\Packages\PowerShellWixExtension.2.0.1\tools\lib\PowerShellWixExtension.dll" Product.wxs
+"%Wix%\bin\light.exe" -out bin\Debug\AutopilotBranding.msi -ext "%ProgramFiles%\PackageManagement\NuGet\Packages\PowerShellWixExtension.2.0.1\tools\lib\PowerShellWixExtension.dll" obj\Debug\Product.wixobj
