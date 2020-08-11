412d38071f639ab81e63746c28214cf4d09b7ae9
Michael Niehaus <niehaus@live.com>
2020-08-11 12:46:08 -0700

    Enable location services when time zone isn't specified
    Added logic to enable location services when a time zone value is not specified in the config.xml.  If the user signing in has admin rights, Windows will attempt to determine the user's location and set the time zone automatically.

--- AutopilotBranding/AutopilotBranding.ps1
+++ AutopilotBranding/AutopilotBranding.ps1
@@ -44,6 +44,12 @@ if ($config.Config.TimeZone) {
 	Write-Host "Setting time zone: $($config.Config.TimeZone)"
 	Set-Timezone -Id $config.Config.TimeZone
 }
+else {
+	# Enable location services so the time zone will be set automatically (even when skipping the privacy page in OOBE) when an administrator signs in
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type "String" -Value "Allow" -Force
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type "DWord" -Value 1 -Force
+	Start-Service -Name "lfsvc" -ErrorAction SilentlyContinue
+}
 
 # STEP 4: Remove specified provisioned apps if they exist
 Write-Host "Removing specified in-box provisioned apps"
