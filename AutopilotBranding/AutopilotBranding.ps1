6af1ade1bb6ceaa5117e7b6162ac8bb70d913b1e
Michael Niehaus <niehaus@live.com>
2025-04-16 22:47:33 -0700

    Lots of stuff

--- AutopilotBranding/AutopilotBranding.ps1
+++ AutopilotBranding/AutopilotBranding.ps1
@@ -43,46 +43,87 @@ reg.exe load HKLM\TempUser "C:\Users\Default\NTUSER.DAT" | Out-Host
 # STEP 1: Apply a custom start menu and taskbar layout
 $ci = Get-ComputerInfo
 if ($ci.OsBuildNumber -le 22000) {
-	Log "Importing layout: $($installFolder)Layout.xml"
-	Copy-Item "$($installFolder)Layout.xml" "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml" -Force
+	if ($config.Config.SkipStartLayout -ine "true") {
+		Log "Importing layout: $($installFolder)Layout.xml"
+		Copy-Item "$($installFolder)Layout.xml" "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml" -Force
+	} else {
+		Log "Skipping Start layout (Windows 10)"
+	}
 } else {
-	Log "Importing Start menu layout: $($installFolder)Start2.bin"
-	MkDir -Path "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState" -Force -ErrorAction SilentlyContinue | Out-Null
-	Copy-Item "$($installFolder)Start2.bin" "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\Start2.bin" -Force
-	Log "Importing Taskbar layout: $($installFolder)TaskbarLayoutModification.xml"
-	MkDir -Path "C:\Windows\OEM\" -Force -ErrorAction SilentlyContinue | Out-Null
-	Copy-Item "$($installFolder)TaskbarLayoutModification.xml" "C:\Windows\OEM\TaskbarLayoutModification.xml" -Force
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v LayoutXMLPath /t REG_EXPAND_SZ /d "%SystemRoot%\OEM\TaskbarLayoutModification.xml" /f | Out-Host
-	Log "Unpin the Microsoft Store app from the taskbar"
-	reg.exe add "HKLM\TempUser\Software\Policies\Microsoft\Windows\Explorer" /v NoPinningStoreToTaskbar /t REG_DWORD /d 1 /f | Out-Host
+	if ($config.Config.SkipStartLayout -ine "true") {
+		Log "Importing Start menu layout: $($installFolder)Start2.bin"
+		MkDir -Path "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState" -Force -ErrorAction SilentlyContinue | Out-Null
+		Copy-Item "$($installFolder)Start2.bin" "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\Start2.bin" -Force
+	} else {
+		Log "Skipping Start layout (Windows 11)"
+	}
+	if ($config.Config.SkipTaskbarLayout -ine "true") {
+		Log "Importing Taskbar layout: $($installFolder)TaskbarLayoutModification.xml"
+		MkDir -Path "C:\Windows\OEM\" -Force -ErrorAction SilentlyContinue | Out-Null
+		Copy-Item "$($installFolder)TaskbarLayoutModification.xml" "C:\Windows\OEM\TaskbarLayoutModification.xml" -Force
+		reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v LayoutXMLPath /t REG_EXPAND_SZ /d "%SystemRoot%\OEM\TaskbarLayoutModification.xml" /f | Out-Host
+		Log "Unpin the Microsoft Store app from the taskbar"
+		reg.exe add "HKLM\TempUser\Software\Policies\Microsoft\Windows\Explorer" /v NoPinningStoreToTaskbar /t REG_DWORD /d 1 /f | Out-Host
+	} else {
+		Log "Skipping Taskbar layout (Windows 11)"
+	}
 }
 
 # STEP 2: Configure background
-Log "Setting up Autopilot theme"
-Mkdir "C:\Windows\Resources\OEM Themes" -Force | Out-Null
-Copy-Item "$installFolder\Autopilot.theme" "C:\Windows\Resources\OEM Themes\Autopilot.theme" -Force
-Mkdir "C:\Windows\web\wallpaper\Autopilot" -Force | Out-Null
-Copy-Item "$installFolder\Autopilot.jpg" "C:\Windows\web\wallpaper\Autopilot\Autopilot.jpg" -Force
-Log "Setting Autopilot theme as the new user default"
-reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v InstallTheme /t REG_EXPAND_SZ /d "%SystemRoot%\resources\OEM Themes\Autopilot.theme" /f | Out-Host
-reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v CurrentTheme /t REG_EXPAND_SZ /d "%SystemRoot%\resources\OEM Themes\Autopilot.theme" /f | Out-Host
+if ($config.Config.SkipTheme -ine "true") {
+	Log "Setting up Autopilot theme"
+	Mkdir "C:\Windows\Resources\OEM Themes" -Force | Out-Null
+	Copy-Item "$installFolder\Autopilot.theme" "C:\Windows\Resources\OEM Themes\Autopilot.theme" -Force
+	Mkdir "C:\Windows\web\wallpaper\Autopilot" -Force | Out-Null
+	Copy-Item "$installFolder\Autopilot.jpg" "C:\Windows\web\wallpaper\Autopilot\Autopilot.jpg" -Force
+	Log "Setting Autopilot theme as the new user default"
+	reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v InstallTheme /t REG_EXPAND_SZ /d "%SystemRoot%\resources\OEM Themes\Autopilot.theme" /f | Out-Host
+	reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v CurrentTheme /t REG_EXPAND_SZ /d "%SystemRoot%\resources\OEM Themes\Autopilot.theme" /f | Out-Host
+} else {
+	Log "Skipping Autopilot theme"
+}
 
-# STEP 2A: Stop Start menu from opening on first logon
-reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v StartShownOnUpgrade /t REG_DWORD /d 1 /f | Out-Host
+# STEP 2A: Set lock screen image, see https://www.systemcenterdudes.com/apply-custom-lock-screen-wallpaper-using-intune/
+if ($config.Config.SkipLockScreen -ine "true") {
+	Log "Configuring lock screen image"
+	$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+	$LockScreenImage = "C:\Windows\web\wallpaper\Autopilot\AutopilotLock.jpg"
+	Copy-Item "$installFolder\AutopilotLock.jpg" $LockScreenImage -Force
+	if (!(Test-Path -Path $RegPath)) {
+		New-Item -Path $RegPath -Force | Out-Null
+	}
+	New-ItemProperty -Path $RegPath -Name LockScreenImagePath -Value $LockScreenImage -PropertyType String -Force | Out-Null
+	New-ItemProperty -Path $RegPath -Name LockScreenImageUrl -Value $LockScreenImage -PropertyType String -Force | Out-Null
+	New-ItemProperty -Path $RegPath -Name LockScreenImageStatus -Value 1 -PropertyType DWORD -Force | Out-Null
 
-# STEP 2B: Hide "Learn more about this picture" from the desktop
-reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" /t REG_DWORD /d 1 /f | Out-Host
+	# STEP 2B: Stop Start menu from opening on first logon
+	reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v StartShownOnUpgrade /t REG_DWORD /d 1 /f | Out-Host
 
-# STEP 2C: Disable Windows Spotlight as per https://github.com/mtniehaus/AutopilotBranding/issues/13#issuecomment-2449224828
-Log "Disabling Windows Spotlight for Desktop"
-reg.exe add "HKLM\TempUser\Software\Policies\Microsoft\Windows\CloudContent" /v DisableSpotlightCollectionOnDesktop /t REG_DWORD /d 1 /f | Out-Host
+	# STEP 2C: Hide "Learn more about this picture" from the desktop (so wallpaper will work)
+	reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" /t REG_DWORD /d 1 /f | Out-Host
 
-# STEP 2D: Left Align Start Button in the default user profile, users can change it if they want
+	# STEP 2D: Disable Windows Spotlight as per https://github.com/mtniehaus/AutopilotBranding/issues/13#issuecomment-2449224828 (so wallpaper will work)
+	Log "Disabling Windows Spotlight for Desktop"
+	reg.exe add "HKLM\TempUser\Software\Policies\Microsoft\Windows\CloudContent" /v DisableSpotlightCollectionOnDesktop /t REG_DWORD /d 1 /f | Out-Host
+
+} else {
+	Log "Skipping lock screen image"
+}
+
+# STEP 2E: Left Align Start Button in the default user profile, users can change it if they want
 if ($config.Config.SkipLeftAlignStart -ine "true") {
 	Log "Configuring left aligned Start menu"
-	reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /f | Out-Host
+	reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /f | Out-Host
 } else {
 	Log "Skipping Left align start"
+}
+
+# STEP 2F: Hide the widgets button
+if ($config.Config.SkipHideWidgets -ine "true") {
+	Log "Hiding widget button"
+	reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f | Out-Host
+} else {
+	Log "Skipping Hide widget button"
 }
 
 # STEP 3: Set time zone (if specified)
@@ -131,7 +172,7 @@ if ($config.Config.OneDriveSetup) {
 		Log "Cleaning up user OneDriveSetup key"
 		Remove-ItemProperty -Path "HKLM:\TempUser\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup" | Out-Null
 		Log "Creating new OneDriveSetup key and pointing it to the machine wide EXE"
-		New-ItemProperty -Path "HKLM:\DefaultUser\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup" -Value '"C:\Program Files\Microsoft OneDrive\Onedrive.exe" /background' -Force
+		New-ItemProperty -Path "HKLM:\TempUser\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDriveSetup" -Value '"C:\Program Files\Microsoft OneDrive\Onedrive.exe" /background' -Force | Out-Null
 	}
 }
 
@@ -160,18 +201,7 @@ if ($currentWU -eq 1)
 	Restart-Service wuauserv
 }
 
-# Step 9A: Add features on demand
-if ($config.Config.AddFeatures.Feature.Count -gt 0)
-{
-	$config.Config.AddFeatures.Feature | % {
-		Log "Adding Windows feature: $_"
-		try {
-			Add-WindowsCapability -Online -Name $_
-		} catch {}
-	}
-}
-
-# Step 9B: Disable Optional features
+# Step 9A: Disable Optional features
 if ($config.Config.DisableOptionalFeatures.Feature.Count -gt 0)
 {
 	$EnabledOptionalFeatures = Get-WindowsOptionalFeature -Online | Where-Object {$_.State -eq "Enabled"}
@@ -179,13 +209,13 @@ if ($config.Config.DisableOptionalFeatures.Feature.Count -gt 0)
 		if ($config.Config.DisableOptionalFeatures.Feature -contains $EnabledFeature.FeatureName) {
 			Log "Disabling Optional Feature:  $($EnabledFeature.FeatureName)"
 			try {
-				Disable-WindowsOptionalFeature -Online -FeatureName $EnabledFeature.FeatureName -NoRestart 
+				Disable-WindowsOptionalFeature -Online -FeatureName $EnabledFeature.FeatureName -NoRestart | Out-Null
 			} catch {}
 		}
 	}
 }
 
-# Step 9C: Remove Windows Capabilities
+# Step 9B: Remove Windows Capabilities
 if ($config.Config.RemoveCapability.Capability.Count -gt 0)
 {
 	$InstalledCapabilities = Get-WindowsCapability -Online | Where-Object {$_.State -eq "Installed"}
@@ -196,6 +226,20 @@ if ($config.Config.RemoveCapability.Capability.Count -gt 0)
 				Remove-WindowsCapability -Online -Name $InstalledCapability.Name  | Out-Null
 			} catch {}
 		}
+	}
+}
+
+# Step 9C: Add features on demand
+if ($config.Config.AddFeatures.Feature.Count -gt 0)
+{
+	$config.Config.AddFeatures.Feature | % {
+		Log "Adding Windows feature: $_"
+		try {
+			$result = Add-WindowsCapability -Online -Name $_
+			if ($result.RestartNeeded) {
+				Log "  Feature $_ was installed but requires a restart"
+			}
+		} catch {}
 	}
 }
 
@@ -213,9 +257,11 @@ if ($config.Config.DefaultApps) {
 }
 
 # STEP 11: Set registered user and organization
-Log "Configuring registered user information"
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOwner /t REG_SZ /d "$($config.Config.RegisteredOwner)" /f /reg:64 | Out-Host
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOrganization /t REG_SZ /d "$($config.Config.RegisteredOrganization)" /f /reg:64 | Out-Host
+if ($config.Config.RegisteredOwner) {
+	Log "Configuring registered user information"
+	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOwner /t REG_SZ /d "$($config.Config.RegisteredOwner)" /f /reg:64 | Out-Host
+	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOrganization /t REG_SZ /d "$($config.Config.RegisteredOrganization)" /f /reg:64 | Out-Host
+}
 
 # STEP 12: Configure OEM branding info
 if ($config.Config.OEMInfo)
@@ -232,7 +278,7 @@ if ($config.Config.OEMInfo)
 }
 
 # STEP 13: Enable UE-V
-if ($config.Config.SkipUEV -ine "false") 
+if ($config.Config.SkipUEV -ine "true") 
 {
 	Log "Enabling UE-V"
 	Enable-UEV
@@ -253,37 +299,52 @@ reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" 
 Log "Turning off Edge desktop icon"
 reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v "CreateDesktopShortcutDefault" /t REG_DWORD /d 0 /f /reg:64 | Out-Host
 
-# STEP 16: if PowerShell version 2 is installed, it’s possible to bypass the constrained language mode,
-# which normally is being enforced by application control solutions like AppLocker and similar.
-Log "Disabling PowerShell v2.0"
-try {
-    $PoShv2Enabled = Get-WindowsOptionalFeature -FeatureName "MicrosoftWindowsPowerShellV2Root" -Online | Select-Object -ExpandProperty State
-} catch {
-    Write-Error "Failed to get the state of the PowerShell v2.0 feature: : $($_.Exception.Message)"
-}
-if ($PoShv2Enabled -eq "Enabled") {
-    try {
-        Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root -ErrorAction Continue -NoRestart
-    } catch {
-        Write-Error "Failed to disable the PowerShell v2.0 feature: $($_.Exception.Message)"
-    }
-}
-
-# STEP 17: Remove the registry keys for Dev Home and Outlook New
+# STEP 16: Remove the registry keys for Dev Home and Outlook New
 # This is a workaround for the issue where the Dev Home and Outlook New apps are installed by default
 Log "Disabling Windows 11 Dev Home and Outlook New"
 $DevHome = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\Orchestrator\UScheduler_Oobe\DevHomeUpdate"
 $OutlookNew = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\Orchestrator\UScheduler_Oobe\OutlookUpdate"
 if (Test-Path -Path $DevHome) {
-    Log "Found --> Removing DevHome"
+    Log "  Removing DevHome key"
     Remove-Item -Path $DevHome -Force
 }
 if (Test-Path -Path $OutlookNew) {
-    Log "Found --> Removing Outlook NEW"
+    Log "  Removing Outlook for Windows key"
     Remove-Item -Path $OutlookNew -Force
 }
 
+# STEP 17: WinGet installs
+if ($config.Config.SkipWinGet -ine "true") {
+	$winget = (Get-ChildItem -Path "C:\Program Files\WindowsApps" -Recurse -Filter "winget.exe").FullName
+	$config.Config.WinGetInstall.Id | % {
+		Log "Installing $_"
+		try {
+			& $winget install "$_" --accept-package-agreements --accept-source-agreements --scope machine
+		} catch {}
+	}
+}
+
+# STEP 18: Try to get Windows to update stuff
+if ($config.Config.SkipLeftAlignStart -ine "true") {
+	try {
+		Log "Updating in-box apps"
+		Install-Script Update-InboxApp
+		Get-AppxPackage | Select-Object -Unique PackageFamilyName | Update-InboxApp.ps1
+
+		Log "Kicking off a Windows Update scan"
+		$Namespace = "Root\cimv2\mdm\dmmap"
+		$ClassName = "MDM_EnterpriseModernAppManagement_AppManagement01"
+		Get-CimInstance -Namespace $Namespace -ClassName $ClassName |
+		Invoke-CimMethod -MethodName UpdateScanMethod
+	} catch {}
+} else {
+	Log "Skipping updates"
+}
+
 # CLEANUP: Unload the default registry profile
+[GC]::Collect()
 reg.exe unload HKLM\TempUser | Out-Host
+
+Write-Host "All done!"
 
 Stop-Transcript
