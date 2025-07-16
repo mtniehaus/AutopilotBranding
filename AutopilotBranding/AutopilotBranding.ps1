14e3b1e463fd8383abbf9c8f2962534f5d6d4445
Michael Niehaus <niehaus@live.com>
2025-07-15 20:26:04 -0500

    Add Edge RemoveDesktopShortcutDefault

--- AutopilotBranding/AutopilotBranding.ps1
+++ AutopilotBranding/AutopilotBranding.ps1
@@ -289,6 +289,7 @@ Log "Turning off (old) Edge desktop shortcut"
 & reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v DisableEdgeDesktopShortcutCreation /t REG_DWORD /d 1 /f /reg:64 2>&1 | Out-Null
 Log "Turning off Edge desktop icon"
 & reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v "CreateDesktopShortcutDefault" /t REG_DWORD /d 0 /f /reg:64 2>&1 | Out-Null
+& reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v "RemoveDesktopShortcutDefault" /t REG_DWORD /d 1 /f /reg:64 2>&1 | Out-Null
 
 # STEP 8A: Attempt to Donwload and install Latest Edge for Business MSI from Evergreen URL
 if ($config.Config.SkipEdgeUpdate -ine "true") {
